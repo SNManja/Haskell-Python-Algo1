497d777e84ea547f722f8ebb382385f601118739
Santiago M <122291289+MonkiManja@users.noreply.github.com>
2023-04-10 23:31:02 -0300

    Descripcion del EX-2 y agregado de archivos de c/u de los ej

---
+++ Practica 3/EX-2.hs
@@ -0,0 +1,8 @@
+{- Especificar e implementar las siguientes funciones, incluyendo su signatura -}
+
+{-
+    Ej 2
+    Aca no voy a notar al menos por el momento la especificacion, solo voy a realizar la implementacion
+-}
+
+

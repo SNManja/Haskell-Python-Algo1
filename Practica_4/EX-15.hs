65bb450eab02db4f9361f4e97b38f16e5fe45271
Santiago M <122291289+MonkiManja@users.noreply.github.com>
2023-04-17 22:40:35 -0300

    EX-15 minor fix

--- Practica_4/EX-15.hs
+++ Practica_4/EX-15.hs
@@ -5,3 +5,6 @@ sumatoriaM p q = fromIntegral (div p q) + sumatoriaM p (q-1)
 sumatoriaN :: Integer -> Integer -> Float
 sumatoriaN 1 q = sumatoriaM 1 q
 sumatoriaN p q = sumatoriaM p q + sumatoriaN (p-1) q
+
+sumaRacionales :: Integer -> Integer -> Float
+sumaRacionales p q = sumatoriaN p q

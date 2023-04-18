b0d6ea1d767f4f7a8fa97a0ac5d4d3d27ddb005b
Santiago M <122291289+MonkiManja@users.noreply.github.com>
2023-04-17 22:26:05 -0300

    CONFUSION hice el 14, no el 13 (aun)

--- Practica_4/EX-13.hs
+++
@@ -1,6 +0,0 @@
-sumaPotencias :: Integer -> Integer -> Integer -> Integer
-sumaPotencias q n m = potencias q n + potencias q m 
-
-potencias :: Integer -> Integer -> Integer
-potencias q 1 = q
-potencias q x = (q^x) + potencias q (x-1) 

f49ea0d0e60b815d08531b8ae3c547ac97206304
Santiago M <122291289+MonkiManja@users.noreply.github.com>
2023-04-17 22:12:14 -0300

    EX-11 corregido y b hecho

--- Practica_4/EX-11.hs
+++ Practica_4/EX-11.hs
@@ -1,7 +1,10 @@
-eAprox :: Float -> Float
+eAprox :: Integer -> Float
 eAprox 0 = 1
-eAprox n = (1 / (fact n)) + eAprox n-1
+eAprox n = (1 / (fact (fromIntegral n))) + eAprox (n-1)
 
 fact :: Float -> Float 
 fact 1 = 1
-fact n = n * fact n-1 
+fact n = n * fact (n-1) 
+
+e :: Float
+e = eAprox 10

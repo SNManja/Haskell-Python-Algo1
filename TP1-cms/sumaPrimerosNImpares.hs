16c9d20d6e445510aba88244167e6ed4d47e5fd7
Santiago M <122291289+MonkiManja@users.noreply.github.com>
2023-04-22 19:18:50 -0300

    sumaPrimerosNImpares hecho? a revisar

--- TP1-cms/sumaPrimerosNImpares.hs
+++ TP1-cms/sumaPrimerosNImpares.hs
@@ -5,9 +5,14 @@ main = do {
   print(sumaPrimerosNImpares(x ::(Integer)))
   }
 
+
+-- sumaPrimerosNImpares era un ejercicio de la guia si no mal recuerdo
+-- En este caso, la especificacion del pdf es diferente 
 sumaPrimerosNImpares :: Integer -> Integer
--- Completar la definición de la función
+sumaPrimerosNImpares n = sumaPrimerosNImparesAux (2*n-1)
 
--- Pueden agregan las funciones que consideren necesarias
-
+sumaPrimerosNImparesAux :: Integer -> Integer
+sumaPrimerosNImparesAux 1 = (2 * 1) + 2
+sumaPrimerosNImparesAux i | mod i 2 == 0 = sumaPrimerosNImparesAux (i-1)
+                          | otherwise = ((2 * i) + 2) + sumaPrimerosNImparesAux (i-2)
 
